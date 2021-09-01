0211cca9c312d6c39a8fec0a82095f8a8dc035d3
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-09-01 08:26:24 +0100

    crud api using drf

---
+++ api/models.py
@@ -0,0 +1,12 @@
+# this is just a test model which you can change to your preferred model(s)
+
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+  title = models.CharField(max_length=200)
+  completed = models.BooleanField(default=False, blank=True, null=True)
+      
+  def __str__(self):
+    return self.title
